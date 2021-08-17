b6f4f1c0e43e7bc7d876c793e62648102f2600d4
sumeet-07 <sumeetpatond@gmail.com>
2021-08-18 00:46:15 +0530

    first question

---
+++ 232 - Implement Queue using Stacks.js
@@ -0,0 +1,66 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function () {
+  this.stack1 = [];
+  this.stack2 = [];
+};
+
+/**
+ * Push element x to the back of queue.
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  for (let i = this.stack2.length; i > 0; i--) {
+    const item = this.stack2[i - 1];
+    this.stack1.push(item);
+  }
+  this.stack2 = [];
+  this.stack1.push(x);
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  for (let i = this.stack1.length; i > 0; i--) {
+    const item = this.stack1[i - 1];
+    this.stack2.push(item);
+  }
+  this.stack1 = [];
+  return this.stack2.pop();
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  for (let i = this.stack1.length; i > 0; i--) {
+    const item = this.stack1[i - 1];
+    this.stack2.push(item);
+  }
+  this.stack1 = [];
+  const item = this.stack2[this.stack2.length - 1];
+  return item;
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  if (this.stack1.length || this.stack2.length) return false;
+  return true;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
